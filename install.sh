2d3d84fbfd8621fb394c2e2c5cf7b71ddeb4c573
Mathias Uhl <mathiasuhl@gmx.de>
2020-12-14 21:48:10 +0000

    fixes

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 sudo touch /etc/i2c_server
 sudo mkdir /usr/local/lib/i2c_server
 sudo touch /var/log/i2c_server.log
-sudo copy i2c_server.conf.yaml /etc/i2c_server
-sudo copy i2c_server.py /usr/local/lib/i2c_server
-sudo copy i2c_server.service /lib/systemd/system/i2c_server.service
+sudo cp i2c_server.conf.yaml /etc/i2c_server
+sudo cp -R * /usr/local/lib/i2c_server
+sudo cp i2c_server.service /lib/systemd/system/i2c_server.service
 sudo systemctl daemon-reload
 sudo systemctl enable i2c_server.service
 sudo service i2c_server start
