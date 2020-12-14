5c3e5e9e3bcb87b742f59cc5ce43d0f5e49260bd
Mathias Uhl <m.uhl@ibdigital.de>
2020-12-14 19:38:09 +0100

    refactor

--- i2c_server.py
+++ i2c_server.py
@@ -2,10 +2,7 @@ import time
 import board
 import busio
 
-from i2c_module_adafruit_shtc3 import Shtc3
-from i2c_module_adafruit_pct2075 import Pct2075
-from i2c_module_adafruit_pct2075 import Lps2x
-from i2c_module_adafruit_bh1750 import Bh1750
+from .active_modules/* import *
 from i2c_server_mqtt import MqttClient
 
 with open("/etc/i2c_server/i2c_server.conf.yaml", "r") as ymlfile:
@@ -14,12 +11,13 @@ with open("/etc/i2c_server/i2c_server.conf.yaml", "r") as ymlfile:
 i2c = busio.I2C(board.SCL, board.SDA)
 mqtt_cient = MqttClient(cfg["mqtt"])
 i2c_sensors = {}
+
 for sensor in cfg["active_sensors"]:
     constructor = globals()[sensor]
     i2c_sensors[sensor.lower()] = constructor()
 
 while True:
-  foreach(module):
-    mqtt_cient.publish(module.sensor_data)
-    module.print()
+  for sensor in i2c_sensors:
+    mqtt_cient.publish(sensor.data)
+    sensor.print()
     time.sleep(3)
