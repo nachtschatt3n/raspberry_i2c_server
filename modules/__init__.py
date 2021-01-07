25d014a54cd8669ccb88c45a3ecbd9529884f903
mathias uhl <mathias.uhl@dlh.de>
2021-01-07 15:02:06 +0100

    small fixes

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,4 +2,3 @@ from .i2c_module_adafruit_bh1750  import Bh1750
 from .i2c_module_adafruit_lps2x   import Lps2x
 from .i2c_module_adafruit_pct2075 import Pct2075
 from .i2c_module_adafruit_shtc3   import Shtc3
-from .i2c_module_adafruit_ssd1306   import Ssd1306
